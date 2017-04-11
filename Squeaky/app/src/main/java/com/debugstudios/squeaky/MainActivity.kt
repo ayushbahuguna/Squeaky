c7a0bfcafddb5fbb1bde8d89443391ec2825a22d
Prajjwal <prajjwaldimri@hotmail.com>
2017-04-11 20:57:34 +0530

    Added MVP Architecture, Closes #5

--- Squeaky/app/src/main/java/com/debugstudios/squeaky/MainActivity.kt
+++ Squeaky/app/src/main/java/com/debugstudios/squeaky/MainActivity.kt
@@ -2,10 +2,20 @@ package com.debugstudios.squeaky
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.debugstudios.squeaky.presenters.MainPresenter
+import com.debugstudios.squeaky.contracts.views.MainView
 import kotlinx.android.synthetic.main.activity_main.*
-import javax.inject.Inject
+import net.grandcentrix.thirtyinch.TiActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : TiActivity<MainPresenter, MainView>(), MainView {
+
+    override fun showText(text: String) {
+        text2.text = text
+    }
+
+    override fun providePresenter(): MainPresenter {
+        return MainPresenter()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
